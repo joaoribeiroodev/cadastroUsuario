96faeb06fa046069d8abe9ac5f05444c0c7c9fbf
Gabriel200617 <gastofoye@gmail.com>
2026-03-19 20:53:28 -0300

    Adicionando DAO

--- src/java/dao/cadastroDAO.java
+++ src/java/dao/cadastroDAO.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package dao;
 
-/**
- *
- * @author 232.003676
- */
-public class cadastroDAO {
+import connection.ConnectionFactory;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.UserModel;
+
+public class cadastroDAO { // Por convenção, nomes de classe em Java começam com letra maiúscula
     
-}
+    // 1. Criamos um MÉTODO que recebe o objeto usuario
+    public boolean cadastrar(UsuarioModel usuario) {
+        
+        String sql = "INSERT INTO users (nome, cpf, email, passwords) VALUES (?, ?, ?, ?)";
+        
+        try (var con = ConnectionFactory.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+            
+            // 2. Usamos o objeto 'usuario' que chegou como parâmetro
+            stmt.setString(1, usuario.getNome());
+            stmt.setString(2, usuario.getCpf());
+            stmt.setString(3, usuario.getEmail());
+            stmt.setString(4, usuario.getPassword()); 
+            
+            int linhasAfetadas = stmt.executeUpdate();
+            
+            return linhasAfetadas > 0;
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false; 
+        }
+    } // Fim do método
+} // Fim da classe
